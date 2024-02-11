cbca2280775f5672b1f16f00e55d5a6872a3a4bf
Santoso Wijaya <code@swijaya.com>
2024-02-11 22:07:52 +0000

    Normalize badge params before handing them off to Badge c'tor

--- lib/jekyll-badges/generator.rb
+++ lib/jekyll-badges/generator.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'date'
 require_relative 'badge.rb'
 
 module JekyllBadges
@@ -10,10 +11,25 @@ module JekyllBadges
     def generate(site)
       badges_data = site.data['badges']
       badges_data.each do |badge_data|
-        badge = Badge.new(**(badge_data.transform_keys(&:to_sym)))
+        badge = Badge.new(**normalize_badge_params(badge_data))
         puts badge.inspect
       end
-      # TODO
     end # def generate
+
+    private
+
+    def normalize_badge_params(badge_data)
+      # Make sure all fields are there; or else provide default values
+      # TODO
+
+      # Make sure `badge_data['image_url']` is valid and the asset exists
+      # TODO
+
+      badge_data['created'] = Date.parse(badge_data['created'])
+      badge_data['granted'] = Date.parse(badge_data['granted'])
+
+      return badge_data.transform_keys(&:to_sym)
+    end
+
   end # class Generator
 end # module JekyllBadges
