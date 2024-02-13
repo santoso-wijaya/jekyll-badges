5d6748b0c969ae7e21feedae6a0353fa041bc279
Santoso Wijaya <code@swijaya.com>
2024-02-13 01:28:45 +0000

    Simplify `tag` module
    By refactoring most of its logic into a `render` module.

---
+++ lib/jekyll-badges/renderer.rb
@@ -0,0 +1,67 @@
+require_relative 'badge.rb'
+
+module Jekyll
+  module Badges
+    class Renderer
+
+      def initialize(site)
+        @site = site
+      end
+
+      def render_badges
+        if disabled_in_development?
+          Jekyll.logger.info "Jekyll Badges:", "Skipping badges generation in development"
+          return ""
+        end
+
+        # TODO: Make sure badge IDs are all unique.
+        badges = @site.data['badges'].map { |badge_data| 
+          Badge.new(**normalize_badge_params(badge_data))
+        }.filter { |badge|
+          badge.show and (not badge.granted.nil?) and (badge.granted <= TODAY)
+        }
+        .sort
+        .reverse
+
+        puts badges.inspect # TODO: debug only; remove
+
+        # get the template and render it 
+        info = {
+          registers: { site: @site }
+        }
+        badges_template.render!(@site.site_payload, info)
+      end
+
+      private
+
+      TODAY = Date.today
+      DEFAULT_TEMPLATE_PATH = '_includes/badges.html'
+
+      # Returns the plugin's config or an empty hash if not set
+      def config
+        @config ||= @site.config["badges"] || {}
+      end
+
+      def badges_template
+        template_fpath = @site.in_source_dir(config['template'] || DEFAULT_TEMPLATE_PATH)
+        partial = File.read(template_fpath)
+        Liquid::Template.parse(partial)
+      end
+
+      def disabled_in_development?
+        config && config["disable_in_development"] && Jekyll.env == "development"
+      end
+
+      def normalize_badge_params(badge_data)
+        # Make sure `badge_data['image_url']` is valid and the asset exists
+        # TODO
+
+        badge_data['created'] = Date.parse(badge_data['created']) unless badge_data['created'].nil?
+        badge_data['granted'] = Date.parse(badge_data['granted']) unless badge_data['granted'].nil?
+
+        return badge_data.transform_keys(&:to_sym)
+      end
+
+    end
+  end
+end
--- lib/jekyll-badges/tag.rb
+++ lib/jekyll-badges/tag.rb
@@ -3,7 +3,7 @@
 require 'date'
 require 'jekyll'
 
-require_relative 'badge.rb'
+require_relative 'renderer.rb'
 
 module Jekyll
   module Badges
@@ -12,67 +12,12 @@ module Jekyll
 
       def initialize(tag_name, content, tokens)
         super
-        @content = content # TODO: add options handling here
+        # TODO: define and handle tag options?
       end
 
       # See: https://github.com/Shopify/liquid/wiki/Liquid-for-Programmers#rendering
       def render(context)
-        @context = context
-
-        if disabled_in_development?
-          Jekyll.logger.info "Jekyll Badges:", "Skipping badges generation in development"
-          return ""
-        end
-
-        # TODO: Make sure badge IDs are all unique.
-        badges = site.data['badges'].map { |badge_data| 
-          Badge.new(**normalize_badge_params(badge_data))
-        }.filter { |badge|
-          badge.show and (not badge.granted.nil?) and (badge.granted <= TODAY)
-        }
-        .sort
-        .reverse
-
-        puts badges.inspect # TODO: debug only; remove
-
-        # get the template
-        badges_template_partial = File.read(badges_template)
-        badges_template_parsed = Liquid::Template.parse(badges_template_partial)
-
-        puts badges_template_parsed.inspect # TODO: debug only; remove
-        
-        return badges_template_parsed.render(context)
-      end
-
-      private
-
-      TODAY = Date.today
-
-      def site
-        @site ||= @context.registers[:site]
-      end
-
-      # Returns the plugin's config or an empty hash if not set
-      def config
-        @config ||= site.config["badges"] || {}
-      end
-
-      def badges_template
-        return site.in_source_dir(config['template'])
-      end
-
-      def disabled_in_development?
-        config && config["disable_in_development"] && Jekyll.env == "development"
-      end
-
-      def normalize_badge_params(badge_data)
-        # Make sure `badge_data['image_url']` is valid and the asset exists
-        # TODO
-
-        badge_data['created'] = Date.parse(badge_data['created']) unless badge_data['created'].nil?
-        badge_data['granted'] = Date.parse(badge_data['granted']) unless badge_data['granted'].nil?
-
-        return badge_data.transform_keys(&:to_sym)
+        Jekyll::Badges::Renderer.new(context.registers[:site]).render_badges
       end
 
     end
